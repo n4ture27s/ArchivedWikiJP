98d5653f1557658fa6bde01f111bfa1dc3962d47
n4ture27s <149782669+n4ture27s@users.noreply.github.com>
2026-05-02 02:06:01 +0900

    feat:weapon list 途中

---
+++ javascript/log.js
@@ -0,0 +1,31 @@
+
+//filter
+function LogFilter(type) {
+    const details = document.querySelectorAll(".update-log details")
+
+    details.forEach(d => {
+        const logs = d.querySelectorAll(".log");
+        let view = 0;
+
+        logs.forEach(log => {
+            if (type === "all" || log.classList.contains(type)) {
+                log.style.display = "block"
+                view++;
+            } else {
+                log.style.display = "none"
+            }
+        })
+
+        d.style.display = view > 0 ? "block" : "none";
+    })
+}
+//filter-2
+const buttons = document.querySelectorAll('.filter-buttons button');
+buttons.forEach(button => {
+    button.addEventListener('click', (ev) => {
+        buttons.forEach(allb => {
+            allb.classList.remove('pressed')
+        })
+        button.classList.add("pressed")
+    })
+})
